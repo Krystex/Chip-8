0c0db99b9ef3992589959370efb51f81a9979750
Krystex <5840423+Krystex@users.noreply.github.com>
2018-02-23 00:49:52 +0100

    Added all missing instructions to parser Restructured parsing test

--- src/instruction.rs
+++ src/instruction.rs
@@ -73,6 +73,36 @@ pub enum Instruction {
 	Shl(Nibble, Nibble),
 	/// Skip next instruction if Vx != Vy.
 	SneReg(Nibble, Nibble),
+	/// Set I = nnn.
+	LdI(Addr),
+	/// Jump to location nnn + V0.
+	JpV0(Addr),
+	/// Set Vx = random byte AND kk.
+	Rnd(Nibble, Byte),
+	/// Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision.
+	Drw(Nibble, Nibble, Nibble),
+	/// Skip next instruction if key with the value of Vx is pressed.
+	Skp(Nibble),
+	/// Skip next instruction if key with the value of Vx is not pressed.
+	Sknp(Nibble),
+	/// Set Vx = delay timer value.
+	LdDelayTimerValue(Nibble),
+	/// Wait for a key press, store the value of the key in Vx.
+	LdKeypress(Nibble),
+	/// Set delay timer = Vx.
+	LdDelayTimerReg(Nibble),
+	/// Set sound timer = Vx.
+	LdSoundTimer(Nibble),
+	/// Set I = I + Vx.
+	AddI(Nibble),
+	/// Set I = location of sprite for digit Vx.
+	LdSprite(Nibble),
+	/// Store BCD representation of Vx in memory locations I, I+1, and I+2.
+	LdBCD(Nibble),
+	/// Store registers V0 through Vx in memory starting at location I.
+	LdStoreV0(Nibble),
+	/// Read registers V0 through Vx from memory starting at location I.
+	LdReadV0(Nibble),
 }
 
 fn get_x(val: u16) -> Nibble {
@@ -87,6 +117,9 @@ fn get_addr(val: u16) -> Addr {
 fn get_byte(val: u16) -> Byte {
 	(val & 0x00ff) as u8
 }
+fn get_nibble(val: u16) -> Nibble {
+	(val & 0x000f) as u8
+}
 
 impl Instruction {
 	/// Get the instruction to the value
@@ -98,6 +131,8 @@ impl Instruction {
 			(val & 0x00f0) >> 4,
 			(val & 0x000f) >> 0
 		);
+		// For debugging
+		println!("{:X}", val);
 		match x {
 			(0x0, 0x0, 0xe, 0x0) => Some(Cls),
 			(0x0, 0x0, 0xe, 0xe) => Some(Ret),
@@ -184,64 +219,118 @@ impl Instruction {
 				let y = get_y(val);
 				Some(SneReg(x, y))
 			},
+			(0xa, _  , _  , _ ) => {
+				let addr = get_addr(val);
+				Some(LdI(addr))
+			},
+			(0xb, _  , _  , _ ) => {
+				let addr = get_addr(val);
+				Some(JpV0(addr))
+			},
+			(0xc, _  , _  , _ ) => {
+				let x = get_x(val);
+				let y = get_byte(val);
+				Some(Rnd(x, y))
+			},
+			(0xd, _  , _  , _ ) => {
+				let x = get_x(val);
+				let y = get_y(val);
+				let n = get_nibble(val);
+				Some(Drw(x, y, n))
+			},
+			(0xe, _ , 0x9, 0xe) => {
+				let x = get_x(val);
+				Some(Skp(x))
+			},
+			(0xe, _ , 0xa, 0x1) => {
+				let x = get_x(val);
+				Some(Sknp(x))
+			},
+			(0xf, _ , 0x0, 0x7) => {
+				let x = get_x(val);
+				Some(LdDelayTimerValue(x))
+			},
+			(0xf, _ , 0x0, 0xa) => {
+				let x = get_x(val);
+				Some(LdKeypress(x))
+			},
+			(0xf, _ , 0x1, 0x5) => {
+				let x = get_x(val);
+				Some(LdDelayTimerReg(x))
+			},
+			(0xf, _ , 0x1, 0x8) => {
+				let x = get_x(val);
+				Some(LdSoundTimer(x))
+			},
+			(0xf, _ , 0x1, 0xe) => {
+				let x = get_x(val);
+				Some(AddI(x))
+			},
+			(0xf, _ , 0x2, 0x9) => {
+				let x = get_x(val);
+				Some(LdSprite(x))
+			},
+			(0xf, _ , 0x3, 0x3) => {
+				let x = get_x(val);
+				Some(LdBCD(x))
+			},
+			(0xf, _ , 0x5, 0x5) => {
+				let x = get_x(val);
+				Some(LdStoreV0(x))
+			},
+			(0xf, _ , 0x6, 0x5) => {
+				let x = get_x(val);
+				Some(LdReadV0(x))
+			},
 			_ => unimplemented!()
 		}
 	}
 }
 
+#[allow(unused_macros)]
+macro_rules! test_instr {
+	($x:expr, $y:expr) => {
+		let test = Instruction::parse($x).unwrap();
+		assert_eq!(test, $y);
+	}
+}
 
 #[test]
 fn test_instructions() {
 	use self::Instruction::*;
 
-	let cls = Instruction::parse(0x00e0).unwrap();
-	assert_eq!(cls, Cls);
-
-	let ret = Instruction::parse(0x00ee).unwrap();
-	assert_eq!(ret, Ret);
-
-	let jp = Instruction::parse(0x1111).unwrap();
-	assert_eq!(jp, Jp(0x0111));
-
-	let call = Instruction::parse(0x2111).unwrap();
-	assert_eq!(call, Call(0x0111));
-
-	let se = Instruction::parse(0x3111).unwrap();
-	assert_eq!(se, Se(0x1, 0x11));
-
-	let sne = Instruction::parse(0x4111).unwrap();
-	assert_eq!(sne, Sne(0x1, 0x11));
-
-	let se_reg = Instruction::parse(0x5110).unwrap();
-	assert_eq!(se_reg, SeReg(0x1, 0x1));
-
-	let ld = Instruction::parse(0x6111).unwrap();
-	assert_eq!(ld, Ld(0x1, 0x11));
-
-	let addreg = Instruction::parse(0x7111).unwrap();
-	assert_eq!(addreg, AddReg(0x1, 0x11));
-
-	let ldreg = Instruction::parse(0x8110).unwrap();
-	assert_eq!(ldreg, LdReg(0x1, 0x1));
-
-	let or = Instruction::parse(0x8111).unwrap();
-	assert_eq!(or, Or(0x1, 0x1));
-
-	let and = Instruction::parse(0x8112).unwrap();
-	assert_eq!(and, And(0x1, 0x1));
-
-	let xor = Instruction::parse(0x8113).unwrap();
-	assert_eq!(xor, Xor(0x1, 0x1));
-
-	let add = Instruction::parse(0x8114).unwrap();
-	assert_eq!(add, AddCarry(0x1, 0x1));
-
-	let sub = Instruction::parse(0x8115).unwrap();
-	assert_eq!(sub, Sub(0x1, 0x1));
-
-	let shr = Instruction::parse(0x8116).unwrap();
-	assert_eq!(shr, Shr(0x1, 0x1));
-
-	let subn = Instruction::parse(0x8117).unwrap();
-	assert_eq!(subn, Subn(0x1, 0x1));
+	test_instr!(0x00e0, Cls);
+	test_instr!(0x00ee, Ret);
+	test_instr!(0x1111, Jp(0x0111));
+	test_instr!(0x2111, Call(0x0111));
+	test_instr!(0x3111, Se(0x1, 0x11));
+	test_instr!(0x4111, Sne(0x1, 0x11));
+	test_instr!(0x5110, SeReg(0x1, 0x1));
+	test_instr!(0x6111, Ld(0x1, 0x11));
+	test_instr!(0x7111, AddReg(0x1, 0x11));
+	test_instr!(0x8110, LdReg(0x1, 0x1));
+	test_instr!(0x8111, Or(0x1, 0x1));
+	test_instr!(0x8112, And(0x1, 0x1));
+	test_instr!(0x8113, Xor(0x1, 0x1));
+	test_instr!(0x8114, AddCarry(0x1, 0x1));
+	test_instr!(0x8115, Sub(0x1, 0x1));
+	test_instr!(0x8116, Shr(0x1, 0x1));
+	test_instr!(0x8117, Subn(0x1, 0x1));
+	test_instr!(0x811e, Shl(0x1, 0x1));
+	test_instr!(0x9110, SneReg(0x1, 0x1));
+	test_instr!(0xa000, LdI(0x000));
+	test_instr!(0xb000, JpV0(0x000));
+	test_instr!(0xc111, Rnd(0x1, 0x11));
+	test_instr!(0xd123, Drw(0x1, 0x2, 0x3));
+	test_instr!(0xe19e, Skp(0x1));
+	test_instr!(0xe1a1, Sknp(0x1));
+	test_instr!(0xf107, LdDelayTimerValue(0x1));
+	test_instr!(0xf10a, LdKeypress(0x1));
+	test_instr!(0xf115, LdDelayTimerReg(0x1));
+	test_instr!(0xf118, LdSoundTimer(0x1));
+	test_instr!(0xf11e, AddI(0x1));
+	test_instr!(0xf129, LdSprite(0x1));
+	test_instr!(0xf133, LdBCD(0x1));
+	test_instr!(0xf155, LdStoreV0(0x1));
+	test_instr!(0xf165, LdReadV0(0x1));
 }
